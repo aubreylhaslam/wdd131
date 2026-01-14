19855aaafe8bf08c80f7f82fa56be697ecba46c6
aubreylhaslam <aubrey@souths.org>
2026-01-14 14:42:56 -0700

    Handling DOM Events ROUGH

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -4,7 +4,17 @@ const list = document.querySelector('#list');
 const li = document.createElement('li');
 const deleteButton = document.createElement('button');
 
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        list.append(li);
+    }
+});
+
+deleteButton.addEventListener('click', function () {
+    list.removeChild(li);
+    input.focus();
+    input.value = '';
+});
