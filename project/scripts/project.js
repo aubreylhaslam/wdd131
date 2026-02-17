c725afe4218b0a797c6cf4e3d8a371f09f3be37e
aubreylhaslam <aubrey@souths.org>
2026-02-17 13:10:37 -0700

    checkmark 1

---
+++ project/scripts/project.js
@@ -0,0 +1,9 @@
+const hamburgerButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamburgerButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamburgerButton.classList.toggle('open');
+});
+
+const invitations = []
